c10b597203c45f09f21d5c05afd96fb913a0385b
kevin <kevinjcord@gmail.com>
2025-03-29 18:39:19 -0500

    added react-router

--- adversacare/src/App.jsx
+++ adversacare/src/App.jsx
@@ -3,19 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Button } from '@mui/material'
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/Layout';
+import LandingPage from './pages/LandingPage';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <p className = "text-red-900">Hello World</p>
-      </div> 
-
-      <Button variant = "contained">
-        Hello
-      </Button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LandingPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
