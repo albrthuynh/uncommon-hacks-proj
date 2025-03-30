78ef89944d059dfec72b2c1a52e79388f25bee40
kevin <kevinjcord@gmail.com>
2025-03-30 05:53:26 -0500

    added oauth

--- adversacare/src/main.jsx
+++ adversacare/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+<Auth0Provider
+    domain="dev-fuj3ts5ye0n7toqw.us.auth0.com"
+    clientId="wWZcMkxu03QXb0dFfIax51TCw61hqRmF"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </StrictMode>,
+  </Auth0Provider>,
 )
--- adversacare/src/pages/SelectRole.jsx
+++ adversacare/src/pages/SelectRole.jsx
@@ -1,11 +1,14 @@
 import SelectCard from "../components/SelectCard";
 import { BiHealth } from "react-icons/bi";
 import {IoPersonOutline} from "react-icons/io5";
+import { useAuth0 } from "@auth0/auth0-react";
+
 function SelectRolePage() {
+    const { loginWithRedirect } = useAuth0();
 
     return (
         <>
-            <div className = "flex flex-row px-5 gap-10 w-full h-full justify-center items-center mt-20">
+            <div className = "flex flex-row gap-10 w-full h-full justify-center items-center mt-20">
                 <SelectCard
                     title = "Patient"
                     icon = {<IoPersonOutline className = "h-45 w-45"/>}
@@ -17,7 +20,7 @@ function SelectRolePage() {
                     title = "Doctor"
                     icon = {<BiHealth className = "h-45 w-45"/>}
                     description = "Manage your patients and track their medications quickly and easily."
-                    destination = "/doctor-signup"
+                    onClick ={() => loginWithRedirect()}
                 />
             </div>
         </>
